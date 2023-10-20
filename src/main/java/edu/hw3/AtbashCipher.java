c0e661660eb399e398e4fc7a66f6ef20a5d14986
lsn03 <lvov.sn@mail.ru>
2023-10-20 14:45:13 +1000

    done Atbash

---
+++ src/main/java/edu/hw3/AtbashCipher.java
@@ -0,0 +1,38 @@
+package edu.hw3;
+
+public class AtbashCipher {
+
+    private static final int LOWER_OFFSET = 97;
+    private static final int MIN_LOWER_VALUE = 97;
+    private static final int MAX_LOWER_VALUE = 122;
+    private static final int HIGH_OFFSET = 65;
+    private static final int MIN_HIGH_VALUE = 65;
+    private static final int MAX_HIGH_VALUE = 90;
+    private static final int LENGHT_OF_LATIN_ALPHABET = 90;
+    public String atbash(String word) {
+
+
+        char[] newWord = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            char symbol = word.charAt(i);
+
+            int currentOffset = 0;
+
+            if (symbol <= MAX_LOWER_VALUE && symbol >= MIN_LOWER_VALUE) {
+                currentOffset = LOWER_OFFSET;
+            } else if (symbol >= MIN_HIGH_VALUE && symbol <= MAX_HIGH_VALUE) {
+                currentOffset = HIGH_OFFSET;
+            }
+
+            if (currentOffset == 0) {
+                newWord[i] = symbol;
+                continue;
+            }
+
+            char newSymbol = (char) ( ( LENGHT_OF_LATIN_ALPHABET -  ((int)symbol - currentOffset ))+currentOffset);
+
+            newWord[i] = newSymbol;
+        }
+        return new String(newWord);
+    }
+}
