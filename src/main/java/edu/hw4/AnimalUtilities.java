43890fcffbc1b457567c373b24c2c045a15e2e72
lsn03 <lvov.sn@mail.ru>
2023-10-26 23:51:29 +1000

    added 3-5 tasks

--- src/main/java/edu/hw4/AnimalUtilities.java
+++ src/main/java/edu/hw4/AnimalUtilities.java
@@ -1,6 +1,8 @@
 package edu.hw4;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class AnimalUtilities {
 
@@ -27,5 +29,25 @@ public final class AnimalUtilities {
             }
 
         }).limit(k).toList();
+    }
+
+    public static Map<Animal.Type, Integer> getCounterOfAnimalTypeTask3(List<Animal> list) {
+        return list.stream().collect(Collectors.groupingBy(Animal::type, Collectors.summingInt(elem -> 1)));
+    }
+
+    public static Animal getTheLongestName(List<Animal> list) {
+        return list.stream().max((animal1, animal2) -> Integer.compare(animal1.name().length(), animal2.name().length())).orElse(null);
+    }
+    public static Animal.Sex getTheMostPopularSex(List<Animal> list) {
+        long males = list.stream().filter(animal -> animal.sex() == Animal.Sex.M).count();
+        long females = list.stream().filter(animal -> animal.sex() == Animal.Sex.F).count();
+
+        if(males>=females){
+            return Animal.Sex.M;
+        }else {
+            return Animal.Sex.F;
+        }
+
+
     }
 }
--- src/test/java/edu/hw4/AnimalUtilitiesTest.java
+++ src/test/java/edu/hw4/AnimalUtilitiesTest.java
@@ -3,6 +3,7 @@ package edu.hw4;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import java.util.HashMap;
 import java.util.List;
 
 public class AnimalUtilitiesTest {
@@ -45,5 +46,88 @@ public class AnimalUtilitiesTest {
         }
     }
 
+    @Test
+    public void testThatTypeCounterIsCorrect() {
+//        Arrange
+        List<Animal> list = List.of(new Animal("Zyblik", Animal.Type.BIRD, Animal.Sex.M, 10, 154, 25, true),
+                new Animal("Fantik", Animal.Type.CAT, Animal.Sex.F, 15, 90, 15, true),
+                new Animal("Gosha", Animal.Type.SPIDER, Animal.Sex.M, 3, 120, 19, true),
+                new Animal("Petya", Animal.Type.SPIDER, Animal.Sex.M, 4, 120, 45, true),
+                new Animal("Maksim", Animal.Type.DOG, Animal.Sex.M, 1, 120, 35, true),
+                new Animal("Bebra", Animal.Type.FISH, Animal.Sex.M, 2, 120, 105, true)
+        );
 
+//        Act
+        var mapResult = AnimalUtilities.getCounterOfAnimalTypeTask3(list);
+        var mapExpected = new HashMap<Animal.Type, Integer>();
+
+        mapExpected.put(Animal.Type.SPIDER, 2);
+        mapExpected.put(Animal.Type.BIRD, 1);
+        mapExpected.put(Animal.Type.CAT, 1);
+        mapExpected.put(Animal.Type.DOG, 1);
+        mapExpected.put(Animal.Type.FISH, 1);
+//        Assert
+        assertEquals(mapExpected, mapResult);
+    }
+
+    @Test
+    public void testThatMaxNameLenghtCorrect() {
+//        Arrange
+        Animal expectedAnimal = new Animal("Zyblik", Animal.Type.BIRD, Animal.Sex.M, 10, 154, 25, true);
+        List<Animal> list = List.of(expectedAnimal,
+                new Animal("Fantik", Animal.Type.CAT, Animal.Sex.F, 15, 90, 15, true),
+                new Animal("Gosha", Animal.Type.SPIDER, Animal.Sex.M, 3, 120, 19, true),
+                new Animal("Petya", Animal.Type.SPIDER, Animal.Sex.M, 4, 120, 45, true),
+                new Animal("Maksim", Animal.Type.DOG, Animal.Sex.F, 1, 120, 35, true),
+                new Animal("Bebra", Animal.Type.FISH, Animal.Sex.M, 2, 120, 105, true)
+        );
+
+//        Act
+        Animal res = AnimalUtilities.getTheLongestName(list);
+
+//        Assert
+        assertEquals(expectedAnimal, res);
+    }
+
+    @Test
+    public void testThatSexMCounterIsCorrectCorrect() {
+//        Arrange
+        Animal zyblik = new Animal("Zyblik", Animal.Type.BIRD, Animal.Sex.M, 10, 154, 25, true);
+        Animal.Sex sexExpected = Animal.Sex.M;
+        List<Animal> list = List.of(zyblik,
+                new Animal("Fantik", Animal.Type.CAT, Animal.Sex.F, 15, 90, 15, true),
+                new Animal("Gosha", Animal.Type.SPIDER, Animal.Sex.M, 3, 120, 19, true),
+                new Animal("Petya", Animal.Type.SPIDER, Animal.Sex.M, 4, 120, 45, true),
+                new Animal("Maksim", Animal.Type.DOG, Animal.Sex.F, 1, 120, 35, true),
+                new Animal("Bebra", Animal.Type.FISH, Animal.Sex.M, 2, 120, 105, true)
+        );
+
+//        Act
+        Animal.Sex sexRes = AnimalUtilities.getTheMostPopularSex(list);
+
+//        Assert
+        assertEquals(sexExpected, sexRes);
+
+
+    }
+    @Test
+    public void testThatSexFCounterIsCorrectCorrect() {
+//        Arrange
+        Animal zyblik = new Animal("Zyblik", Animal.Type.BIRD, Animal.Sex.F, 10, 154, 25, true);
+        Animal.Sex sexExpected = Animal.Sex.F;
+        List<Animal> list = List.of(zyblik,
+                new Animal("Fantik", Animal.Type.CAT, Animal.Sex.F, 15, 90, 15, true),
+                new Animal("Gosha", Animal.Type.SPIDER, Animal.Sex.F, 3, 120, 19, true),
+                new Animal("Petya", Animal.Type.SPIDER, Animal.Sex.M, 4, 120, 45, true),
+                new Animal("Maksim", Animal.Type.DOG, Animal.Sex.F, 1, 120, 35, true),
+                new Animal("Bebra", Animal.Type.FISH, Animal.Sex.M, 2, 120, 105, true)
+        );
+
+//        Act
+        Animal.Sex sexRes = AnimalUtilities.getTheMostPopularSex(list);
+
+//        Assert
+        assertEquals(sexExpected, sexRes);
+
+    }
 }
