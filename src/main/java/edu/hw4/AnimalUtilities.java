ab6211a40f61e10f2b8efce585b8fa7a0b5f2f8d
lsn03 <lvov.sn@mail.ru>
2023-10-26 22:44:41 +1000

    add task2

--- src/main/java/edu/hw4/AnimalUtilities.java
+++ src/main/java/edu/hw4/AnimalUtilities.java
@@ -1,21 +1,31 @@
 package edu.hw4;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 public final class AnimalUtilities {
 
     private AnimalUtilities() {
     }
-    public static List<Animal> sortHeight(List<Animal> list){
+
+    public static List<Animal> sortHeightTask1(List<Animal> list) {
         return list.stream().sorted((o1, o2) -> {
-            if(o1.height()>o2.height()){
+            if (o1.height() > o2.height()) {
                 return 1;
-            }else {
-                return o1.height() < o2.height()? -1 : 0;
+            } else {
+                return o1.height() < o2.height() ? -1 : 0;
             }
 
         }).toList();
+    }
+
+    public static List<Animal> sortWeightTask2(List<Animal> list, int k) {
+        return list.stream().sorted((o1, o2) -> {
+            if (o1.weight() < o2.weight()) {
+                return 1;
+            } else {
+                return o1.weight() > o2.weight() ? -1 : 0;
+            }
+
+        }).limit(k).toList();
     }
 }
--- src/test/java/edu/hw4/AnimalUtilitiesTest.java
+++ src/test/java/edu/hw4/AnimalUtilitiesTest.java
@@ -7,18 +7,43 @@ import java.util.List;
 
 public class AnimalUtilitiesTest {
     @Test
-    public void testThatHeightSortedCorrect() {
+    public void testThatHeightSortedCorrectTask1() {
 //        Arrange
-        List<Animal> list = List.of(new Animal("a", Animal.Type.BIRD, Animal.Sex.F, 10, 100, 25, true),
-                new Animal("a", Animal.Type.CAT, Animal.Sex.F, 10, 90, 25, true),
-                new Animal("a", Animal.Type.SPIDER, Animal.Sex.M, 10, 120, 25, true)
+        List<Animal> list = List.of(new Animal("Zyblik", Animal.Type.BIRD, Animal.Sex.M, 10, 154, 25, true),
+                new Animal("Fantik", Animal.Type.CAT, Animal.Sex.F, 10, 90, 25, true),
+                new Animal("Gosha", Animal.Type.SPIDER, Animal.Sex.M, 10, 120, 25, true)
         );
 //        Act
-        list = AnimalUtilities.sortHeight(list);
+        list = AnimalUtilities.sortHeightTask1(list);
 //        Assert
-        for (int i = 0; i < list.size()-1; i++) {
-            assertTrue(list.get(i).height()  <list.get(i+1).height() );
+        for (int i = 0; i < list.size() - 1; i++) {
+            assertTrue(list.get(i).height() < list.get(i + 1).height());
         }
-
     }
+
+    @Test
+    public void testThatWeightSortedCorrectTask2() {
+//        Arrange
+        List<Animal> list = List.of(new Animal("Zyblik", Animal.Type.BIRD, Animal.Sex.M, 10, 154, 25, true),
+                new Animal("Fantik", Animal.Type.CAT, Animal.Sex.F, 15, 90, 15, true),
+                new Animal("Gosha", Animal.Type.SPIDER, Animal.Sex.M, 3, 120, 19, true),
+                new Animal("Petya", Animal.Type.SPIDER, Animal.Sex.M, 4, 120, 45, true),
+                new Animal("Maksim", Animal.Type.DOG, Animal.Sex.M, 1, 120, 35, true),
+                new Animal("Bebra", Animal.Type.FISH, Animal.Sex.M, 2, 120, 105, true)
+        );
+        int k1 = 2;
+        int k2 = 3;
+//        Act
+        var list1 = AnimalUtilities.sortWeightTask2(list, k1);
+        var list2 = AnimalUtilities.sortWeightTask2(list, k2);
+//        Assert
+        for (int i = 0; i < list1.size() - 1; i++) {
+            assertTrue(list1.get(i).weight() >= list1.get(i + 1).weight());
+        }
+        for (int i = 0; i < list2.size() - 1; i++) {
+            assertTrue(list2.get(i).weight() >= list2.get(i + 1).weight());
+        }
+    }
+
+
 }
