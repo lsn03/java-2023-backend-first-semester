0f0fa832cb63acd5a0ea1f6b54947636ea0a69b3
lsn03 <lvov.sn@mail.ru>
2023-11-01 00:40:34 +1000

    fix unused methods

--- src/main/java/edu/hw4/AnimalValidator.java
+++ src/main/java/edu/hw4/AnimalValidator.java
@@ -1,12 +1,9 @@
 package edu.hw4;
 
-
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
